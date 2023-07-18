86379ca6fab9868cf2e66d22d156237e2bcbbaea
EREN <erteneren93@gmail.com>
2023-07-18 13:33:40 +0100

    Initial Commit

---
+++ BrowserApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BrowserApp
+//
+//  Created by Eren Erten on 18.07.2023.
+//
+
+import UIKit
+import BrowserLib
+
+class ViewController: UIViewController {
+
+    var a = BrowserLib()
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
